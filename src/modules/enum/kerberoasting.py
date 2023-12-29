a06694c2944111ee9c10263c2e15c12213204b60
oppsec <opps3c@gmail.com>
2023-12-29 00:09:20 -0300

    fix: minor changes

--- src/modules/enum/kerberoasting.py
+++ src/modules/enum/kerberoasting.py
@@ -5,10 +5,9 @@ def get_kerberoastable() -> None:
     ''' Search for kerberoastable users with filter and ignore krbtgt or computers'''
 
     # TO-DO: Return kerberoastable user hash
-
     # https://github.com/byt3bl33d3r/CrackMapExec/blob/3c3e412193cb6d3237abe90c543e5d995bfa4447/cme/protocols/ldap.py#L927C1-L927C1
+    
     search_filter = "(&(servicePrincipalName=*)(UserAccountControl:1.2.840.113556.1.4.803:=512)" "(!(UserAccountControl:1.2.840.113556.1.4.803:=2))(!(objectCategory=computer)))"
-
     query = connect_and_fetch(search_filter)
 
     if query:
